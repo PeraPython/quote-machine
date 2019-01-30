dcd32ef3a3cb42d20e8c70e13a0dd13028314492
Marko <marko.perendic@yahoo.com>
2019-01-30 16:46:39 +0100

    Updates

---
+++ precache-manifest.a2117a5df2c647ce155fac8f06172190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22742a60cdb878f6f135",
+    "url": "/quote-machine/static/js/runtime~main.22742a60.js"
+  },
+  {
+    "revision": "f46f3c0b8bfd0a2c5f78",
+    "url": "/quote-machine/static/js/main.f46f3c0b.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/quote-machine/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "f46f3c0b8bfd0a2c5f78",
+    "url": "/quote-machine/static/css/main.7a5b6532.chunk.css"
+  },
+  {
+    "revision": "00ffe89b5a9975321d32f83bd6be5e26",
+    "url": "/quote-machine/index.html"
+  }
+];
